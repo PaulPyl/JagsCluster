b1f48f57e346c7e9ef76e83a284ec3ddf38d3804
Paul Theodor Pyl <paul.theodor.pyl@gmail.com>
2017-06-28 13:03:00 +0200

    Updates and bugfixes, reordering clusters by their center when naming.

--- R/clusterSamples.r
+++ R/clusterSamples.r
@@ -1,4 +1,4 @@
-clusterSamples <- function(input, Nclust = 10, nAdapt = 1000, nBurnIn = 2000, nSample = 500, nChain = 1, maxSNVs = 1000, minAF = 0.1, varCF = Inf, minCov = 10){
+clusterSamples <- function(input, Nclust = 10, nAdapt = 1000, nBurnIn = 2000, nSample = 500, nChain = 1, maxSNVs = 1000, minAF = 0.1, varCF = Inf, minCov = 10, minWeight = 0){
   verifyInput(input) # check if input is valid
   input$AF <- input$Support / input$Coverage
   input$AF[!is.finite(input$AF)] <- 0
@@ -9,12 +9,27 @@ clusterSamples <- function(input, Nclust = 10, nAdapt = 1000, nBurnIn = 2000, nS
   input <- lapply(input, function(m) m[selectedSNVs,,drop = FALSE])
   if(sum(selectedSNVs) == 1){
     message("Trivial case with only one SNV passing filter")
+    input$Cluster <- data.frame(
+      "ClusterIdx" = 1,
+      "SNVID"      = rownames(input$Support),
+      stringsAsFactors = FALSE
+    )
+    input$Cluster$ClusterName <- "c1"
     ret <- list(
       "jags.result"    = NA,
       "Modelling.Data" = input,
-      "Clusters"       = c(1)
+      "Clusters"       = data.frame(
+        "Mean" = mean(input$AF),
+        "Var"  = var(input$AF),
+        "toRemove" = FALSE,
+        "Sample" = colnames(input$Support),
+        "clusterIdx" = 1,
+        "Weight" = 1,
+        "ClusterName" = "c1",
+        stringsAsFactors = FALSE
+        ),
+      "ClusterNameMap" = c("c1")
     )
-    names(ret$Clusters) <- rownames(input$Support)[1]
     return(ret)
   }
   if(length(selectedSNVs) > maxSNVs){
@@ -26,20 +41,28 @@ clusterSamples <- function(input, Nclust = 10, nAdapt = 1000, nBurnIn = 2000, nS
   weights <- apply(res$cluster.weight, 1, mean)
   clusters <- do.call(rbind,lapply(seq(ncol(input$Support)), function(i){
     data.frame(
-      "Mean"      = apply(res[[paste0("cluster.center.", i)]], 1, mean),
-      "Var"       = apply(res[[paste0("cluster.center.", i)]], 1, var),
-      "toRemove"  = apply(res[[paste0("cluster.center.", i)]], 1, var) >= varCF,
-      "Sample"    = colnames(input$Support)[i],
-      "Cluster"   = paste0("c", seq(Nclust)),
-      "Weight"    = weights,
+      "Mean"        = apply(res[[paste0("cluster.center.", i)]], 1, mean),
+      "Var"         = apply(res[[paste0("cluster.center.", i)]], 1, var),
+      "toRemove"    = (apply(res[[paste0("cluster.center.", i)]], 1, var) >= varCF) | (weights <= minWeight),
+      "Sample"      = colnames(input$Support)[i],
+      "ClusterIdx"  = seq(Nclust),
+      "Weight"      = weights,
       stringsAsFactors = FALSE
     )
   }))
-  input$Cluster <- apply(res$clust, c(1,3), median)[,1]
-  names(input$Cluster) <- rownames(input$Support)
+  clusterNameMap <- rep(NA, Nclust)
+  clusterNameMap[!subset(clusters, Sample == colnames(input$Support)[1])$toRemove] <- paste0("c", rank(subset(clusters, Sample == colnames(input$Support)[1] & !toRemove)$Mean))
+  clusters$ClusterName <- clusterNameMap[clusters$ClusterIdx]
+  input$Cluster <- data.frame(
+    "ClusterIdx" = apply(res$clust, c(1,3), median)[,1],
+    "SNVID"      = rownames(input$Support),
+    stringsAsFactors = FALSE
+  )
+  input$Cluster$ClusterName <- clusterNameMap[input$Cluster$ClusterIdx]
   list(
     "jags.result"    = res,
     "Modelling.Data" = input,
-    "Clusters"       = clusters
+    "Clusters"       = clusters,
+    "ClusterNameMap" = clusterNameMap
   )
 }
